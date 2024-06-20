c932e87b53238fd20ab3f560099a8b082df74162
Ed Chan <schan3edd@gmail.com>
2024-06-20 12:17:25 -0400

    typing

--- src/commands/create.ts
+++ src/commands/create.ts
@@ -17,7 +17,7 @@ export default class Create extends Command {
         
 
         this.log((await git.branch()).current)
-        var existingBranchesTree;
+        var existingBranchesTree: any
         try {
             existingBranchesTree = JSON.parse(readFileSync('branches.json', 'utf-8'))
         } catch (err) {
